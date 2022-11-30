57a23929189c83ecab5e6117e25e0fcf6e1269a0
Klim Savanovich <k.sa@onerep.com>
2022-11-30 23:20:20 +0300

    feat: output of all files

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,25 @@
+import { promises as fs } from 'fs';
+import path from 'path';
+
 const list = async () => {
-    // Write your code here 
+    // Write your code here
+    try {
+        const pathToDir = (name = '') => path.resolve(name);
+        const allFilesAndDir = await fs.readdir(pathToDir());
+
+        if (!allFilesAndDir.includes('files')) {
+            throw new Error('FS operation failed');
+        }
+
+        const allFiles = await fs.readdir(pathToDir('files'));
+
+        for (const file of allFiles) {
+            // or can use stdout
+            console.log(file.trim());
+        }
+    } catch (err) {
+        throw new Error(err);
+    }
 };
 
 await list();
