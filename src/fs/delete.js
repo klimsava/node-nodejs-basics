18ca3f24b21efa0da1190d722a7fa262e25e5b34
Klim Savanovich <k.sa@onerep.com>
2022-11-30 03:07:12 +0300

    feat: remove file

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,15 @@
+import { promises as fs } from 'fs';
+import path from 'path';
+
 const remove = async () => {
-    // Write your code here 
+    // Write your code here
+    const allFiles = await fs.readdir(path.resolve('files'));
+
+    if (!allFiles.includes('fileToRemove.txt')) {
+        throw new Error('FS operation failed');
+    }
+
+    await fs.unlink(path.resolve('files', 'fileToRemove.txt'));
 };
 
 await remove();
