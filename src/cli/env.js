16508eeba78f307f84616af9d7df6763bd480750
Klim Savanovich <k.sa@onerep.com>
2022-12-02 00:31:35 +0300

    feat: made output of the env RSS_ parameter

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,10 @@
 const parseEnv = () => {
-    // Write your code here 
+    // Write your code here
+    const arrayEnv = Object.entries(process.env);
+
+    const result = arrayEnv.filter(array => array[0].startsWith('RSS_')).map(elem => elem.join("="));
+
+    console.log(result);
 };
 
 parseEnv();
