f369a4379524ecd927bda6e4c5592b916f4ec53a
Klim Savanovich <k.sa@onerep.com>
2022-12-05 23:29:50 +0300

    feat: made write streams task

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,11 @@
+import { createWriteStream } from 'fs';
+import path from 'path';
+import { stdin } from 'process';
+
 const write = async () => {
-    // Write your code here 
+    const writeStream = createWriteStream(path.resolve('files', 'fileToWrite.txt'), 'utf-8');
+
+    stdin.pipe(writeStream);
 };
 
 await write();
