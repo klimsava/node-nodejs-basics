320847aebeadb25beb10f7f52647192ae3f8f575
Klim Savanovich <k.sa@onerep.com>
2022-12-05 22:43:36 +0300

    feat: made made args task

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,25 @@
 const parseArgs = () => {
-    // Write your code here 
+    // Write your code here
+    const arrayArgs = process.argv.slice(2);
+    const resultKey = [];
+    const resultItem = [];
+    let result = [];
+
+    for (const arg of arrayArgs) {
+        if (arg.startsWith('--')) {
+            resultKey.push(arg);
+        } else {
+            resultItem.push(arg);
+        }
+    }
+
+    for (let i = 0; i < resultKey.length; i++) {
+        result.push(`${resultKey[i]} is ${resultItem[i]}`);
+    }
+    
+    result = result.join(', ');
+
+    console.log(result);
 };
 
 parseArgs();
